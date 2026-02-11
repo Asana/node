0f26935ba387c834e441f11f7bd1f9967c9bad27
Jack Strohm <jackstrohm@asana.com>
2026-02-10 16:43:02 -0800

    added fips to the staging script to make sure we don't have cache issues

--- stage_for_s3.bash
+++ stage_for_s3.bash
@@ -10,14 +10,17 @@ TIMESTAMP=$(date '+%Y%m%d.%H%M')
 echo "Current timestamp is $TIMESTAMP"
 
 # Download packages and move to codez
-gh release download -R Asana/node -p "packages_*.gz"
+gh release download -R Asana/node node-v22.21.1-release -p "packages_*.gz"
 echo "Moving packages to $CODEZ/node/gyp"
 mv packages_arm64.tar.gz $CODEZ/node/gyp/packages_arm64_node22.tar.gz
 mv packages_x64.tar.gz $CODEZ/node/gyp/packages_amd64_node22.tar.gz
 
-# Download fibers binaries and node tarballs
-gh release download -R Asana/node -p "linux-*.gz"
-gh release download -R Asana/node -p "*.xz"
+# Download fibers binaries and node tarballs (standard build)
+gh release download -R Asana/node node-v22.21.1-release -p "linux-*.gz"
+gh release download -R Asana/node node-v22.21.1-release -p "*.xz"
+
+# Download FIPS node tarballs
+gh release download -R Asana/node node-v22.21.1-fips-release -p "*.xz"
 
 # Download fibers base package via CloudFront (public S3 access was disabled)
 curl "https://asana-oss-cache.asana.biz/node-fibers/fibers-5.0.4.pc.tgz" --output fibers-5.0.4.tar.gz
@@ -49,11 +52,12 @@ for file in *.tar.xz; do
     echo "Renaming: $file -> $new_name"
     mv "$file" "$new_name"
 
-    if [[ "$new_name" =~ node-v([0-9.]+)-(darwin|linux)-(arm64|x64).*\.tar\.xz$ ]]; then
+    if [[ "$new_name" =~ node-v([0-9.]+)-(fips-)?(darwin|linux)-(arm64|x64).*\.tar\.xz$ ]]; then
       version="${BASH_REMATCH[1]}"
-      os="${BASH_REMATCH[2]}"
-      arch="${BASH_REMATCH[3]}"
-      target_dir="node-v${version}-${os}-${arch}"
+      fips="${BASH_REMATCH[2]}"
+      os="${BASH_REMATCH[3]}"
+      arch="${BASH_REMATCH[4]}"
+      target_dir="node-v${version}-${fips}${os}-${arch}"
 
       echo "Target Dir: $target_dir"
       mkdir "$target_dir"
@@ -82,3 +86,4 @@ cd ..
 mv stage "node-${UNIQUE}"
 
 echo "Files are in node-${UNIQUE}, please upload to s3"
+echo "Packages were moved to $CODEZ/node/gyp/packages_{arm64,amd64}_node22.tar.gz"
